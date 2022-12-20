2be4689cfc166d3ed86aec902b9f59efc51ca8fa
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2022-12-20 11:10:09 +0800

    project

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,11 +1,10 @@
 import { getAllByAltText } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
 
-const token = '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';
+const token = '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';
 
 function Github() {
     const [repos, setRepos] = useState([]);
-
     useEffect(() => {
         async function getRepos() {
             const response = await fetch('https://api.github.com/user/repos', {
@@ -19,12 +18,24 @@ function Github() {
 
         getRepos();
     }, []);
+
     const filteredRepos = repos.filter(repo =>
         repo.visibility == "public"
     );
+    const [loadMore, setloadMore] = useState(3)
+
+    const load = ()=> {setloadMore(prevVal => {
+        if (prevVal >= filteredRepos.length ) {
+            return prevVal = 3
+        } else { 
+            return prevVal + 3
+        }
+    })}
+
+    console.log(loadMore)
+
 
     const [languages, setLanguages] = useState({})
-
     useEffect(() => {
     filteredRepos.map(lang=> {
         fetch(lang.languages_url)
@@ -37,16 +48,16 @@ function Github() {
     let languagesList =  Object.keys(languages)
     // console.log(languagesList)
     return (
-        <div className='box-list'>
+        <div className='box-list' >
             {
-            filteredRepos.map(repo => (
+            filteredRepos.slice(0,loadMore).map(repo => (
                 <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className='main-box-project' key={repo.id}>
                     <div className='link-project'>
                         <a class="fa-solid fa-folder-open"></a>
                         <small>{repo.created_at.slice(0,10)}</small>
                     </div>
                     <div>
-                        <h4>{repo.name}</h4>
+                        <h4 key={repo.id}>{repo.name}</h4>
                         <p>A static website that you can see all the trending Movie, Tv series, anime. Built with HTML, CSS Bootstrap </p>
                     </div>
                     <div className='tools-used'>{    
@@ -56,6 +67,7 @@ function Github() {
                     </div>
                 </a>
             ))}
+            <div className='btn-more'><button onClick={load}>{loadMore >= filteredRepos.length? "See Less": "See More"}</button></div>
         </div>
     );
 }
