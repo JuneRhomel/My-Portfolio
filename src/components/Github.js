ffaa9cbee2d7e1167fba64763e48e5ebb98af951
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2022-12-25 20:42:13 +0800

    text

--- src/components/Github.js
+++ src/components/Github.js
@@ -24,35 +24,37 @@ function Github() {
     );
     const [loadMore, setloadMore] = useState(3)
 
-    const load = ()=> {setloadMore(prevVal => {
-        if (prevVal >= filteredRepos.length ) {
-            return prevVal = 3
-        } else { 
-            return prevVal + 3
-        }
-    })}
+    const load = () => {
+        setloadMore(prevVal => {
+            if (prevVal >= filteredRepos.length) {
+                return prevVal = 3
+            } else {
+                return prevVal + 3
+            }
+        })
+    }
 
-console.log(filteredRepos)
+    console.log(filteredRepos)
 
 
     return (
         <div className='box-list' >
             {
-            filteredRepos.slice(0,loadMore).map(repo => (
-                <a data-aos="fade-up" href={repo.html_url} target="_blank" rel="noopener noreferrer" className='main-box-project' key={repo.id}>
-                    <div className='link-project'>
-                        <a class="fa-solid fa-folder-open"></a>
-                        <small>Last Updated At : {repo.pushed_at.slice(0,10)}</small>
-                    </div>
-                    <div>
-                        <h4 key={repo.id}>{repo.name}</h4>
-                       
-                    </div>
-                    <div className='tools-used'><b>{repo.language}</b>
-                    </div>
-                </a>
-            ))}
-            <div className='btn-more'><button onClick={load}>{loadMore >= filteredRepos.length? "See Less": "See More"}</button></div>
+                filteredRepos.slice(0, loadMore).map(repo => (
+                    <a data-aos="fade-up" href={repo.html_url} target="_blank" rel="noopener noreferrer" className='main-box-project' key={repo.id}>
+                        <div className='link-project'>
+                            <a class="fa-solid fa-folder-open"></a>
+                            <small>Last Updated At : {repo.pushed_at.slice(0, 10)}</small>
+                        </div>
+                        <div>
+                            <h4 key={repo.id}>{repo.name}</h4>
+
+                        </div>
+                        <div className='tools-used'><b>{repo.language}</b>
+                        </div>
+                    </a>
+                ))}
+            <div className='btn-more'><button onClick={load}>{loadMore >= filteredRepos.length ? "See Less" : "See More"}</button></div>
         </div>
     );
 }
