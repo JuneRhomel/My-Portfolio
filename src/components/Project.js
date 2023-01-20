75b43f798e9e7c5da5ad2f49800d76ccf2067ede
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2023-01-20 12:02:04 +0800

    update

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Project1 from "../assets/project1.png"
 import Project2 from "../assets/project2.png"
+import Project3 from "../assets/project3.png"
 import "../style/projects.css"
 
 export default function project() {
@@ -9,6 +10,7 @@ export default function project() {
 
             <div className="container">
                 <h3 className='project-h3'><span>04.</span>Some Things I’ve Built</h3>
+
                 <div data-aos="zoom-out">
                     <div className='box-project'  >
                         <div className='bg-poject'>
@@ -60,6 +62,33 @@ export default function project() {
                         </div>
                     </div>
                 </div>
+                
+                <div data-aos="zoom-out">
+                    <div className='box-project'  >
+                        <div className='bg-poject'>
+                            <div className='bg'>
+                                <div className='overlay'></div>
+                                <img src={Project3} alt="" />
+                            </div>
+                        </div>
+                        <div className='text-project'>
+                            <i className='tag'>Featured Project</i>
+                            <h2>Soria</h2>
+                            <div className='p-box'>
+                                <p>A website where you can easily book a hotel room online with a few simple clicks, allowing you to reserve your desired accommodations and plan your trip with ease. </p>
+                            </div>
+                            <div className='list-tools-used'>
+                            <b>Vs Code</b> <b>CSS</b> <b>React.js</b> <b>PHP</b><b>MySql</b><b>Rest API</b>
+                            </div>
+                            <div className='btn-project'>
+                                <a href='https://github.com/SetSaito/hotel-booking' target="_blank" rel="noopener noreferrer" class="fa-brands fa-github"></a>
+                                <a href='https://soria-hotel.web.app/' target="_blank" rel="noopener noreferrer" class="fa-solid fa-arrow-up-right-from-square"></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </section>
     )
