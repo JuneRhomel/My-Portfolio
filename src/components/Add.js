dd2015822b91317a3d9894db32a6c8459f04b257
June Rhomel <junemandigma@gmailo.com>
2023-02-21 16:19:18 +0800

    form

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,49 +1,97 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import test from "../assets/hero.png"
 
 export default function Add() {
-    const addBtn = () => {
+    const [ShowForm, SetShowForm] = useState(false);
+    const [Tools, setTools] = useState([])
+    const ToolsRef = useRef()
 
+    
+    const handelAdd = () => {
+        const Newtool = {
+            id : Tools.length +1,
+            tool : ToolsRef.current.value
+        } 
+        ToolsRef.current.value = ''
+        setTools([...Tools,Newtool ])
     }
-  return (
-    <div>
-        <button onClick={addBtn} className='add-btn'><i class="fa-solid fa-folder-plus"></i></button>
+    const delBtn = (i) => {
+        const remove =  Tools.filter((item) => i != item.id )
+        setTools(remove)
+    }
+    const [view,SetView] =useState(null)
 
-        <div className='bg-form'>
-            <form action="">
-                <div>
-                    <label htmlFor="">Project Name</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Image</label>
-                    <input className='file'  type="file" />
-                </div>
-                <div>
-                    <label htmlFor="">Github</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Link</label>
-                    <input type="text" />
-                </div>
-                <div className='tools'>
-                    <label htmlFor="">Tools</label>
-                    <div className='list-tools'>
-                        <b>Css</b>
-                        <b>Css</b>
-                        <b>Css</b>
-                    </div>
-                    <div className='tools-btn'>
-                    <input type="text" />
-                    <button>Add</button>
-                    </div>
+    const ViewHandel = (e) => {
+        if(e.target.files["0"].type == "image/jpeg") {
+            SetView(e.target.files["0"])
+        }
+    }
 
+    const handelSubmit = (e) => {
+        e.preventDefault();
+    }
+    
+    return (
+        <div>
+            <button onClick={() => SetShowForm(!ShowForm)} className='add-btn'><i class={ShowForm ? "fa-solid fa-right-from-bracket" : "fa-solid fa-folder-plus"} ></i></button>
+            {ShowForm ?
+                <div className='bg-form'>
+
+                    <form onSubmit={handelSubmit}>
+                        <div>
+                            <label htmlFor="">Project Name</label>
+                            <input type="text" />
+                        </div>
+                        <div className='image-file'>
+                            <div>
+                            <label htmlFor="">Image</label>
+                            <input className='file' type="file"onChange={ViewHandel}/>
+                            </div>
+                            <div className='main-img'>
+                                {view?
+                                <img src={URL.createObjectURL(view)} alt=""  />
+                                    :
+                                    ""
+                                }
+                                
+                            </div>
+                        </div>
+                        <div>
+                            <label htmlFor="">Description</label>
+                            <textarea name="Description"></textarea>
+                        </div>
+                        <div>
+                            <label htmlFor="">Github</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label htmlFor="">Link</label>
+                            <input type="text" />
+                        </div>
+                        <div className='tools'>
+                            <label htmlFor="">Tools</label>
+                            <div className='list-tools'>
+                                {Tools.map((i) => {
+                                    return(
+                                    <samp key={i.id}>
+                                        <b onDoubleClick={() =>delBtn(i.id) }>{i.tool}</b>
+                                    </samp>
+                                    )
+                                })}
+
+                            </div>
+                            <div className='tools-btn'>
+                                <input ref={ToolsRef} type="text" />
+                                <button onClick={handelAdd}>Add</button>
+                            </div>
+
+                        </div>
+                        <div>
+                            <button className='submit' type='submit'>Send</button>
+                        </div>
+                    </form>
                 </div>
-                <div>
-                    <button className='submit' type='submit'>Send</button>
-                </div>
-            </form>
+                : ""}
         </div>
-    </div>
-  )
+    )
 }
